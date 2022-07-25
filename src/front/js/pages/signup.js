2299bbc85a1fc94eb505eafb4460aabdde9dcc9e
kakum3 <kakum_3@hotmail.com>
2022-07-25 18:54:45 +0000

    input signup

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -5,10 +5,23 @@ import "../../styles/home.css";
 
 export const Signup = () => {
   const { store, actions } = useContext(Context);
+  const [values, setValues] = useState({email:'user2',password:"pass"})
+  const handleInputChange = (e) => {
+      setValues({...values,  [e.target.name] : e.target.value})
 
+  }
+  const formSubmit = (e)=> {
+      e.preventDefault()
+      actions.getSignup(values)
+  }
   return (
-    <div className="text-center mt-5">
-      <h1>Signup</h1>
-    </div>
+      <div className="text-center mt-5">
+          <h1>Signup Rigo!!</h1>
+          <form onSubmit={formSubmit}>
+              <input value={values.email} name="email" onChange={handleInputChange} onLoad={handleInputChange} />
+              <input value={values.password} name="password" onChange={handleInputChange} onLoad={handleInputChange}/>
+              <button type="submit" className="btn btn-primary">Enviar</button>
+          </form>
+      </div>
   );
 };
