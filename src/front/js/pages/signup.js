3ebd2e7cfdd55913ba4f616da33c248ad292aad6
kakum3 <kakum_3@hotmail.com>
2022-07-31 06:25:15 +0000

    ahora si

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -1,13 +1,21 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState,useEffect } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { useNavigate } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Signup = () => {
   const { store, actions } = useContext(Context);
-  const [values, setValues] = useState({ email: 'user2', password: "pass" })
+  const [values, setValues] = useState({ email: 'user2', password: "pass" });
+  const navigate = useNavigate();
+  const redir = () => navigate("/login", { replace: true });
+  useEffect(
+
+    () => { if (store.loggedIn === true) redir() }, [store.loggedIn]
+  )
   const handleInputChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value })
+  
 
   }
   const formSubmit = (e) => {
