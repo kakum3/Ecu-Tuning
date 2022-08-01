dbd2b84a952de66abadcb6a46a78413d38d395a8
kakum3 <kakum_3@hotmail.com>
2022-08-01 14:15:29 +0000

    añado boton cerrar sesion

--- src/front/js/pages/protected.js
+++ src/front/js/pages/protected.js
@@ -1,19 +1,21 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import { Navigate, Link } from "react-router-dom";
 
 export const Protected = () => {
-  const { store, actions } = useContext(Context);
-  useEffect(() => {
-    actions.getProtected(actions.getToken()).then((e) => {
-      console.log(e);
-    });
-  }, []);
-  return (
-    <div className="text-center mt-5">
-      {store.loggedIn ? "User logged in" : "Denied access"}
-      <h1>Protected</h1>
-    </div>
-  );
+	const { store, actions } = useContext(Context);
+	useEffect( () => {
+		actions.getProtected(actions.getToken());
+	}, [])
+	const handleSesion = ()=>{
+		actions.removeToken();
+		return(<Navigate to="/" replace={true} />)
+	}
+	return (
+		<div className="text-center mt-5">
+			<h1>Protected: {store.loggedIn.email ? store.loggedIn.pasword : 'Acceso Denegado'}</h1>
+			<Link to="/" className="btn btn-primary" onClick={handleSesion}>Cerrar Sesion</Link>
+		</div>
+	);
 };
+
