da54344a8c6940b4bb30a9e699f28bcf94deba41
Brox13 <a_gomez@hotmail.es>
2022-08-05 18:35:00 +0000

    nabvar enlaces

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,26 +3,30 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+		<nav className="navbar navbar-expand-lg bg-light">
+			<div className="container-fluid">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<a className="navbar-brand" href="#">Fast&Furious</a>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/login">
-						<button className="btn btn-primary">Login</button>
-					</Link>
-					<Link to="/signup">
-						<button className="btn btn-primary">Signup</button>
-					</Link>
-					<Link to="/protected">
-						<button className="btn btn-primary">Protected</button>
-					</Link>
-					<Link to="/demo">
-						<button className="btn btn-primary">Context</button>
-					</Link>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<Link to="/map">
+								<a className="nav-link active" aria-current="page" href="#">Mapas</a>
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/profile">
+								<a className="nav-link" href="#">Usuarios</a>
+							</Link>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</nav>
+
 	);
 };
