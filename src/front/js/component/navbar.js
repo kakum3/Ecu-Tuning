d33c99b5de5b9ab1986a73d0d3edf7cc36798452
Brox13 <a_gomez@hotmail.es>
2022-08-06 11:49:14 +0000

    probando cosas en navbar y footer aunque comentado sin terminar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTools } from '@fortawesome/free-solid-svg-icons'
+
 
 export const Navbar = () => {
 	return (
@@ -15,7 +18,7 @@ export const Navbar = () => {
 					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 						<li className="nav-item">
 							<Link to="/map">
-								<a className="nav-link active" aria-current="page" href="#">Mapas</a>
+								<a className="nav-link active" aria-current="page" href="#"><FontAwesomeIcon icon="fas fa fa-tools" />Mapa</a>
 							</Link>
 						</li>
 						<li className="nav-item">
@@ -26,7 +29,29 @@ export const Navbar = () => {
 					</ul>
 				</div>
 			</div>
-		</nav>
-
+		</nav >
 	);
 };
+
+
+// ste navbar me da error en la linea de <use xlink:href="bootstrap" pero puedo estar bien 
+{/* <nav className="navbar navbar-expand-lg bg-light">
+			<div className="container">
+				<div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+					<a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+						<svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+					</a>
+
+					<ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+						<li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
+						<li><a href="#" className="nav-link px-2 text-white">Features</a></li>
+						<li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
+						<li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
+						<li><a href="#" className="nav-link px-2 text-white">About</a></li>
+					</ul>
+					<div className="text-end">
+						<button type="button" className="btn btn-outline-light me-2">Login</button>
+						<button type="button" className="btn btn-warning">Sign-up</button>
+					</div>
+				</div>
+			</div> */}
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -13,25 +13,35 @@ export const Home = () => {
 				{/* <img src={rigoImageUrl} /> */}
 			</p>
 			<div class="">
-				<select class="form-select fs-3" id="inputGroupSelect02">
+				<select class="form-select fs-5" id="inputGroupSelect02">
 					<option selected="">Marca...</option>
 					<option value="1">Audi</option>
 					<option value="2">BMW</option>
 					<option value="3">Citroen</option>
 				</select>
-				<select class="form-select" id="inputGroupSelect02">
+				<select class="form-select fs-5" id="inputGroupSelect02">
 					<option selected="">Modelo...</option>
 					<option value="1">A1</option>
 					<option value="2">A2</option>
 					<option value="3">A3</option>
 				</select>
-				<select class="form-select" id="inputGroupSelect02">
-					<option selected="">Modelo...</option>
-					<option value="1">A1</option>
-					<option value="2">A2</option>
-					<option value="3">A3</option>
+				<select class="form-select fs-5" id="inputGroupSelect02">
+					<option selected="">Año...</option>
+					<option value="1">2000-2003</option>
+					<option value="2">2003-2009</option>
+					<option value="3">2009-2012</option>
+				</select>
+				<select class="form-select fs-5" id="inputGroupSelect02">
+					<option selected="">Motor...</option>
+					<option value="1">1.6 Tdi 105cv</option>
+					<option value="2">1.9 Tdi 110cv</option>
+					<option value="3">2.0 Tdi 140cv</option>
 				</select>
 			</div>
+			<div class="d-grid gap-2 col-6 mx-auto">
+				<button class="btn btn-primary" type="button">Comprobar</button>
+			</div>
 		</div>
+
 	);
 };
