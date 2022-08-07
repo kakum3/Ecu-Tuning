80bf779baf42b9d40ab09e5ac2f17115029f72d3
LuisAguadoVicaria <luisaguadovicaria@gmail.com>
2022-08-07 18:25:34 +0000

    bootstrap and fontawesome updated. navbar def icon not working

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,6 +1,6 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
@@ -9,4 +9,7 @@ import "../styles/index.css";
 import Layout from "./layout";
 
 //render your react application
-ReactDOM.render(<Layout />, document.querySelector("#app"));
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(
+    <Layout />
+);
