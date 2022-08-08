ee8d5f3b0d90e231c7dd084bed4b2bc58d20a769
Brox13 <a_gomez@hotmail.es>
2022-08-08 18:57:19 +0000

    todo en develop

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,28 +1,24 @@
 import React, { Component } from "react";
-
-
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">Luis, Jesús y Antonio</a>
-		</p>
-	</footer>
-
-	// 	// <div className="container">
-	// 	// 	<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-	// 	// 		<div className="col-md-4 d-flex align-items-center">
-	// 	// 			<a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-	// 	// 				<svg className="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-	// 	// 			</a>
-	// 	// 			<span className="mb-3 mb-md-0 text-muted">© 2022 Fast&Furious</span>
-	// 	// 		</div>
-
-	// 	// 		<ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-	// 	// 			<li className="ms-3"><a className="text-muted" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-	// 	// 			<li className="ms-3"><a className="text-muted" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-	// 	// 			<li className="ms-3"><a className="text-muted" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
-	// 	// 		</ul>
-	// 	// 	</footer>
-	// 	// </div>
+	// <footer className="footer mt-auto py-3 text-center">
+	//  <p>
+	//      Made with <i className="fa fa-heart text-danger" /> by{" "}
+	//      <a href="http://www.4geeksacademy.com">Luis, Jesús y Antonio</a>
+	//  </p>
+	// </footer>
+	<div className="container-fluid text-bg-dark">
+		<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+			<div className="col-md-4 d-flex align-items-center">
+				<a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+					<i class="fa-solid fa-car"></i>
+				</a>
+				<span className="mb-3 mb-md-0 text-muted">© 2022 Fast&Furious</span>
+			</div>
+			<ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+				<li className="ms-3"><a className="text-muted" href="#"><i class="fa-brands fa-facebook"></i></a></li>
+				<li className="ms-3"><a className="text-muted" href="#"><i class="fa-brands fa-instagram"></i></a></li>
+				<li className="ms-3"><a className="text-muted" href="#"><i class="fa-brands fa-twitter"></i></a></li>
+			</ul>
+		</footer>
+	</div>
 );
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,29 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+// export const Navbar = () => {
+//  return (
+//      <nav className="navbar navbar-expand-lg bg-light">
+//          <div className="container-fluid">
+//              <Link to="/">
+//                  <a className="navbar-brand" href="#">Fast&Furious</a>
+//              </Link>
+//              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//                  <span className="navbar-toggler-icon"></span>
+//              </button>
+//              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//                      <li className="nav-item">
+//                          <Link to="/map">
+//                              <a className="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-screwdriver-wrench"></i>Mapa</a>
+//                          </Link>
+//                      </li>
+//                      <li className="nav-item">
+//                          <Link to="/profile">
+//                              <a className="nav-link" href="#"><i class="fa-solid fa-user"></i>Usuarios</a>
+//                          </Link>
+//                      </li>
+//                  </ul>
+//              </div>
+//          </div>
+//      </nav >
+//  );
+// };
+// ste navbar me da error en la linea de <use xlink:href="bootstrap" pero puedo estar bien
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg bg-light">
-			<div className="container-fluid">
-				<Link to="/">
-					<a className="navbar-brand" href="#">Fast&Furious</a>
-				</Link>
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-						<li className="nav-item">
-							<Link to="/map">
-								<a className="nav-link active" aria-current="page" href="#"><i icon="fas fa fa-tools" />Mapa</a>
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/profile">
-								<a className="nav-link" href="#">Usuarios</a>
-							</Link>
-						</li>
+		<nav className="p-3 text-bg-dark">
+			<div className="container">
+				<div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+					<a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+						<i className="fa fa-heart text-danger" />
+					</a>
+					<ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+						<Link to="/">
+							<li><a href="#" className="nav-link px-2 text-secondary"><h5><strong>Fast&Furious</strong></h5></a></li>
+						</Link>
+						<Link to="/map">
+							<li><a href="#" className="nav-link px-2 text-white">Talleres</a></li>
+						</Link>
+						<li><a href="#" className="nav-link px-2 text-white">Contacto</a></li>
+						<li><a href="#" className="nav-link px-2 text-white">Salir</a></li>
 					</ul>
+					<div className="text-end">
+						<Link to="/login">
+							<button type="button" className="btn btn-outline-light me-2">Usiario</button>
+						</Link>
+						<Link to="/signup">
+							<button type="button" className="btn btn-success">Registrar</button>
+						</Link>
+					</div>
+					<div className="text-end">
+						<a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+							<img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle"></img>
+						</a>
+					</div>
 				</div>
 			</div>
 		</nav >
@@ -31,24 +67,9 @@ export const Navbar = () => {
 };
 
 
-// ste navbar me da error en la linea de <use xlink:href="bootstrap" pero puedo estar bien 
-{/* <nav className="navbar navbar-expand-lg bg-light">
-			<div className="container">
-				<div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-					<a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-						<svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-					</a>
 
-					<ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-						<li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
-						<li><a href="#" className="nav-link px-2 text-white">Features</a></li>
-						<li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
-						<li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
-						<li><a href="#" className="nav-link px-2 text-white">About</a></li>
-					</ul>
-					<div className="text-end">
-						<button type="button" className="btn btn-outline-light me-2">Login</button>
-						<button type="button" className="btn btn-warning">Sign-up</button>
-					</div>
-				</div>
-			</div> */}
+
+
+
+
+
