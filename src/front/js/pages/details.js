6d278231d3b61b25f60f5932d5ad34bef56c54c8
kakum3 <kakum_3@hotmail.com>
2022-08-10 09:47:47 +0000

    nuevo diseño

--- src/front/js/pages/details.js
+++ src/front/js/pages/details.js
@@ -1,69 +1,75 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import Servicelist from "../component/servicelist";
+import "../../styles/details.css";
 
 export const Details = () => {
   const { store, actions } = useContext(Context);
 
   return (
+    <div className="container-fuid">
+    
     <div className="row">
-      <div className="col-sm-12 col-lg-5">
+      <div className="col-sm-12 col-lg-5 bg-dark">
+      <h2 className="logo">TALLER <span>Paco</span></h2>
+        <Servicelist />
+        
         <Servicelist />
 
-        <div className="row text-center m-auto ">
-          <div class=" text-center mt-5">
-            <div class="dropdown">
-              <button
-                class="btn btn-secondary dropdown-toggle"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Contactar
-              </button>
-              <ul class="dropdown-menu dropdown-menu-dark">
-                <li>
-                  <a class="dropdown-item active" href="#">
-                    {" "}
-                    <div className="content mb-3">
-                      <label for="exampleFormControlInput1" class="form-label">
-                        Email address
-                      </label>
-                      <input
-                        type="email"
-                        class="form-control"
-                        id="exampleFormControlInput1"
-                        placeholder="name@example.com"
-                      ></input>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    <div class="form-floating">
-                      <textarea
-                        class="form-control"
-                        placeholder="Leave a comment here"
-                        id="floatingTextarea"
-                      ></textarea>
-                      <label for="floatingTextarea">Comments</label>
-                    </div>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
       </div>
       <div className="col-sm-12 col-lg-7">
-        <h1>Nombre taller</h1>
-        <p>calle inventada 2</p>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu9-fcYSUonKnrISp1NP-vEIS09hsykWEK4w&usqp=CAU"
-          class="rounded mx-auto d-block"
-          alt="..."
-        ></img>
+       
+            <div className="content">
+
+<h1 className="logo">Contact <span>Us</span></h1>
+
+<div className="contact-wrapper animated bounceInUp">
+    <div className="contact-form">
+        <h3>Contact us</h3>
+        <form action="">
+            <p>
+                <label>FullName</label>
+                <input type="text" name="fullname"/>
+            </p>
+            <p>
+                <label>Email Adress</label>
+                <input type="email" name="email"/>
+            </p>
+            <p>
+                <label>Phone Number</label>
+                <input type="tel" name="phone"/>
+            </p>
+            <p>
+                <label>Affair</label>
+                <input type="text" name="affair"/>
+            </p>
+            <p className="block">
+               <label>Message</label> 
+                <textarea name="message" rows="3"></textarea>
+            </p>
+            <p className="block">
+                <button>
+                    Send
+                </button>
+            </p>
+        </form>
+    </div>
+    <div className="contact-info">
+        <h4>TALLER PACO</h4>
+        <ul>
+            <li><i className="fas fa-map-marker-alt"></i> Baker Street</li>
+            <li><i className="fas fa-phone"></i> (111) 111 111 111</li>
+            <li><i className="fas fa-envelope-open-text"></i> contact@website.com</li>
+        </ul>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero provident ipsam necessitatibus repellendus?</p>
+        <p>Company.com</p>
+    </div>
+</div>
+</div>
+</div>
+
       </div>
     </div>
+    
   );
 };
