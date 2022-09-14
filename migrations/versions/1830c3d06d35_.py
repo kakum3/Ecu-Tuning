0967a498e3f9cb2ddaef4b391e61e283381fd5aa
Jesús Espartero Perales <kakum_3@hotmail.com>
2022-09-14 18:43:05 +0000

    Ecu Tubnig

--- migrations/versions/1830c3d06d35_.py
+++ migrations/versions/1830c3d06d35_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 0eac15289324
+Revision ID: 1830c3d06d35
 Revises: 
-Create Date: 2022-09-08 18:27:41.864097
+Create Date: 2022-09-14 18:39:19.063279
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0eac15289324'
+revision = '1830c3d06d35'
 down_revision = None
 branch_labels = None
 depends_on = None
